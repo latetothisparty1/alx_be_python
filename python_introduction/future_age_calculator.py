cc670ce2538d1081adc97495f01d2ffaea33be9d
Akram Mathu <mathuakram@gmail.com>
2024-09-13 16:28:46 +0300

    Eigth commit

--- python_introduction/future_age_calculator.py
+++ python_introduction/future_age_calculator.py
@@ -1,4 +1,4 @@
-current_age = input("Enter your current age: ")
+current_age = input("Hold old are you? ")
 future_age = current_age + 27
 
 print("In 2050, you will be " + str(future_age) + " years old.")
