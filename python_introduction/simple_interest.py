50bb8bf2071e9a0fd8b8edaddf459feb2f73388e
Akram Mathu <mathuakram@gmail.com>
2024-09-13 16:00:07 +0300

    Third commit

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -2,4 +2,6 @@ principle = 1000
 rate = 0.05
 time = 3
 
-print("The simple interest is: " + str(principle * rate * time))
+interest = principle * rate * time
+
+print("The simple interest is: " + str(interest))
