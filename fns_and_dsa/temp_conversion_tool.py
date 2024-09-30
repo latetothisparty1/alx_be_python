4975028dc73160c296d9e3017832676f2c01a9f0
Akram Mathu <mathuakram@gmail.com>
2024-09-30 13:53:20 +0300

    Ninth Commit

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -1,6 +1,6 @@
 # Global conversion factors
 FAHRENHEIT_TO_CELSIUS_FACTOR = 5 / 9
-CELSIUS_TO_FAHRENHEIT_FACTOR = 9 / 5  # This was missing
+CELSIUS_TO_FAHRENHEIT_FACTOR = 9 / 5  # Ensure this line is present
 OFFSET = 32  # Offset for Fahrenheit to Celsius conversion
 
 # Function to convert Fahrenheit to Celsius
